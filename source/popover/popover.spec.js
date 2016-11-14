eb136e98fd30de85fec4c77eb1562f13fd82f9a8
Raul Glogovetan <raul@glogovetan.com>
2016-11-14 09:39:40 +0200

    added real data to test otherwise it won't have id data for keys

--- source/popover/popover.spec.js
+++ source/popover/popover.spec.js
@@ -5,6 +5,15 @@ import { expect } from 'chai';
 import Popover from './popover.react.js';
 import Avatar from '../avatar/avatar.react.js';
 
+const data = [
+	{ "src": "media/avatar1.png", "label": "Avatar 1", "id": 1 },
+	{ "src": "media/avatar2.png", "label": "Avatar 2", "id": 2 },
+	{ "src": "media/avatar3.png", "label": "Avatar 3", "id": 3 },
+	{ "src": "media/avatar4.png", "label": "Avatar 4", "id": 4 },
+	{ "src": "media/avatar5.png", "label": "Avatar 5", "id": 5 },
+	{ "src": "media/avatar6.png", "label": "Avatar 6", "id": 6 }
+];
+
 describe('Popover Component', () => {
 
 	it('should have the required props', () => {
@@ -53,7 +62,6 @@ describe('Popover Component', () => {
 
 	it ('should contain 6 child <Avatar /> components', () => {
 
-		const data = [{}, {}, {}, {}, {}, {}];
 		const popover = mount(<Popover data={data} click={(event, position) => {}} />);
 
 		expect(popover.find(Avatar)).to.have.length(6);
